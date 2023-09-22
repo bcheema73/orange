432e5d4032d5c2741d136ea4777521ad9f02e355
Bhupinder Singh <bhupinder@logicproviders.com>
2023-09-22 21:10:01 +0530

    First Commit

---
+++ demo-project/src/main/java/com/example/app/OrangeMain.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+public class OrangeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How is Orange !!!");
+	}
+
+}
